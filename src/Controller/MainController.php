846797a9aef0b83b70332a938c9754559b213c06
Borys35 <boryskac10@gmail.com>
2024-06-13 22:40:35 +0200

    add property entity

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,11 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class MainController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/search', name: 'app_search')]
+    public function search(): Response
+    {
+        return $this->render('home/search.html.twig');
     }
 }
---
+++ src/Entity/Property.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PropertyRepository::class)]
+class Property
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\ManyToOne(inversedBy: 'properties')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?PropertyType $type = null;
+
+    #[ORM\Column(length: 2047, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $area = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $location = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?bool $is_for_sale = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $room_count = null;
+
+    #[ORM\Column(type: Types::ARRAY, nullable: true)]
+    private ?array $photos = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getType(): ?PropertyType
+    {
+        return $this->type;
+    }
+
+    public function setType(?PropertyType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getArea(): ?float
+    {
+        return $this->area;
+    }
+
+    public function setArea(float $area): static
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isForSale(): ?bool
+    {
+        return $this->is_for_sale;
+    }
+
+    public function setForSale(bool $is_for_sale): static
+    {
+        $this->is_for_sale = $is_for_sale;
+
+        return $this;
+    }
+
+    public function getRoomCount(): ?int
+    {
+        return $this->room_count;
+    }
+
+    public function setRoomCount(?int $room_count): static
+    {
+        $this->room_count = $room_count;
+
+        return $this;
+    }
+
+    public function getPhotos(): ?array
+    {
+        return $this->photos;
+    }
+
+    public function setPhotos(?array $photos): static
+    {
+        $this->photos = $photos;
+
+        return $this;
+    }
+}
